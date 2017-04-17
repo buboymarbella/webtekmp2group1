db8f7857097585f375698e089e9752dca08422af
lairde <lairde29@gmail.com>
2017-04-17 09:58:32 +0800

    l

--- SSD/borrowform/ajax/barrower.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (isset($_POST['fname'],$_POST['lname'],$_POST['email'],$_POST['ename'],$_POST['Month'],$_POST['day'],$_POST['year'],
-$_POST['strt_time'],$_POST['end_time'],$_POST['submit'])) {
-	print_r($_POST);
-}	 	 	
